87e04e811d8c9152c45342865ff22c7b42360265
Eranda Upeshitha <upeshitha@gmail.com>
2021-07-16 17:48:47 +0530

    refactoring the render method

--- index.js
+++ index.js
@@ -2,40 +2,37 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
-  renderLeads()
+  render(myLeads)
 }
 
-deleteBtn.addEventListener("dblclick", function() {
-  console.log("double click")
-  localStorage.clear()
-  myLeads = []
-  renderLeads()
-})
-
-inputBtn.addEventListener("click", function(){
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
-  localStorage.setItem("myLeads", JSON.stringify(myLeads))
-  renderLeads()
-  console.log(localStorage.getItem("myLeads"))
-})
-
-function renderLeads() {
+function render(leads) {
   let listItems = ""
-  for (var i = 0; i < myLeads.length; i++) {
+  for (var i = 0; i < leads.length; i++) {
     listItems += `
       <li>
-        <a target='_blank' href=${myLeads[i]}>
-          ${myLeads[i]} 
+        <a target='_blank' href=${leads[i]}>
+          ${leads[i]}
         </a>
       </li>
     `
   }
   ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+inputBtn.addEventListener("click", function(){
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
+})
